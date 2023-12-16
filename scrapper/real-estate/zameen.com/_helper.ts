a60b7749e70bd5a9080062695bdcb9ef80d1170c
Hamza Ali <73743551+HamzaAliKundi@users.noreply.github.com>
2023-12-16 22:44:32 +0500

    set url maker for zameen

--- scrapper/real-estate/zameen.com/_helper.ts
+++ scrapper/real-estate/zameen.com/_helper.ts
@@ -1,4 +1,4 @@
-import { zameenPropertyTypeRent, zameenPropertyTypeBuy, zameenCityUrlEnum } from '../utils/url-helper-enums';
+// import { zameenPropertyTypeRent, zameenPropertyTypeBuy, zameenCityUrlEnum } from '../utils/url-helper-enums';
 
 export const getZameenExtractedCityName = (_city: string) => {
     const cityName = _city.split('-')[0]
@@ -32,6 +32,43 @@ export const makeZameenUrl = (_proptyType: string, _city: string, _page: number)
     const url = `https://www.zameen.com/${_proptyType}/${_city}-${_page}.html?sort=date_desc`;
     return url;
 };
+
+export enum zameenType {
+    "Rent" = "Rentals",
+    "Buy" = "buy",
+}
+
+let zameenPropertyTypeRent: any = {
+    "Home": "Rentals",
+    "Plots": "Rentals_Plots",
+    "Commercial": "Rentals_Commercial"
+}
+
+let zameenPropertyTypeBuy: any = {
+    "Home": "Homes",
+    "Plots": "Plots",
+    "Commercial": "Commercial"
+}
+
+let zameenCityUrlEnum: any = {
+    "Islamabad": "Islamabad-3",
+    "Karachi": "Karachi-2",
+    "Lahore": "Lahore-1",
+    "Rawalpindi": "Rawalpindi-41",
+    "Abbottabad": "Abbottabad-385",
+    "Bahawalpur": "Bahawalpur-23",
+    "Faisalabad": "Faisalabad-16",
+    "Gujranwala": "Gujranwala-327",
+    "Gujrat": "Gujrat-20",
+    "Hyderabad": "Hyderabad-30",
+    "Jhelum": "Jhelum-19",
+    "Multan": "Multan-15",
+    "Murree": "Murree-36",
+    "Peshawar": "Peshawar-17",
+    "Sargodha": "Sargodha-778",
+    "Sialkot": "Sialkot-480",
+    "Wah": "Wah-459",
+}
 
 export const propertTypes = [
     zameenPropertyTypeRent.Home,
